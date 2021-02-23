68d1d4a060ebd307f6d9dcd5158fbff61f83a256
menxipeng <Mxp949544>
2021-02-23 18:12:44 +0800

    feat: 基础配置更新

---
+++ src/test/java/com/mxp/configserver3344/ConfigServer3344ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mxp.configserver3344;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServer3344ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
